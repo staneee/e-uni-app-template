dff377f569b0479a1d0c4074e7d26f2b5b35e138
stan e <yi.hang@live.com>
2019-12-06 23:00:50 +0800

    终于踩完第一波坑了

---
+++ app/android/app/src/main/java/com/staneee/e_uni_app_template_android/wxapi/WXEntryActivity.java
@@ -0,0 +1,8 @@
+//package com.staneee.e_uni_app_template_android.wxapi;
+//
+//import io.dcloud.feature.oauth.weixin.AbsWXCallbackActivity;
+//
+//public class WXEntryActivity extends AbsWXCallbackActivity{
+//
+//}
+//
---
+++ app/android/app/src/main/java/com/staneee/e_uni_app_template_android/wxapi/WXPayEntryActivity.java
@@ -0,0 +1,7 @@
+//package com.staneee.e_uni_app_template_android.wxapi;
+//
+//import io.dcloud.feature.payment.weixin.AbsWXPayCallbackActivity;
+//
+//public class WXPayEntryActivity extends AbsWXPayCallbackActivity{
+//
+//}
